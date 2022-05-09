870cb2d8d05f1b30f732449036dc10d306167e4d
ElaineCosta <elainerscosta@hotmail.com>
2022-05-09 20:12:39 -0300

    iniciando o teste 2 - About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testar o componente About', () => {
+  it('Verificar se a pagina contem informações da Pokedex', () => {
+    
+  });
+
+  it('Verificar se a pagina contem um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const titleEl = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(titleEl).toBeDefined();
+});
+
+it('Verificar se a pagina contem dois parágrafos com texto sobre a Pokédex', () => {
+    render(<About />);
+    const p1El = screen.getByText(/This application simulates a Pokédex/i);
+    const p2El = screen.getByText(/One can filter Pokémons by type/i);
+
+    expect(p1El).toHaveTextContent(/This application simulates a Pokédex, a/,
+      / digital encyclopedia containing all Pokémons/i,);
+
+    expect(p2El).toHaveTextContent(/One can filter Pokémons by type, /,
+      / and see more details for each one of them/i);
+});
+
+it('Verificar se a pagina contem  a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.', () => {
+    
+});
+});
