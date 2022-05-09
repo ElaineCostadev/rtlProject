985068287f2b025051f28022ec32b3ec202b4840
ElaineCosta <elainerscosta@hotmail.com>
2022-05-09 19:21:11 -0300

    requesito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém links de navegação', () => {
+  it('O primeiro link deve possuir o texto Home', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  it('O segundo link deve possuir o texto About', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(linkFavorite).toBeInTheDocument();
+  });
+
+  it('Verifica se ao clicar é redirecionado ao Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(linkHome);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Verifica se ao clicar é redirecionado ao About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    userEvent.click(linkAbout);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Verifica se ao clicar é redirecionado ao Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(linkFavorite);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Verifica se ao clicar é redirecionado ao Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/inexistente');
+    const notFound = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(notFound).toBeDefined();
+  });
+});
